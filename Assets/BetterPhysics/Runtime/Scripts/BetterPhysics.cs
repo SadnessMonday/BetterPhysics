c7430f2c674bcb53e07e1b84baf2652ba7ac22a2
Jeremy Schiff <jeremy@sadnessmonday.com>
2024-12-01 14:03:13 -0500

    Update version in code

--- Assets/BetterPhysics/Runtime/Scripts/BetterPhysics.cs
+++ Assets/BetterPhysics/Runtime/Scripts/BetterPhysics.cs
@@ -3,7 +3,7 @@ using SadnessMonday.BetterPhysics.Layers;
 
 namespace SadnessMonday.BetterPhysics {
     public class BetterPhysics {
-        public const string BetterPhysicsVersion = "1.0.3";
+        public const string BetterPhysicsVersion = "1.0.4";
 
         public static int DefinedLayerCount => BetterPhysicsSettings.Instance.DefinedLayerCount;
         
